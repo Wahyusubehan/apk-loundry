9d18e157617d92cb6b4037e3f5346038da63625f
AlfianIlham27 <alfianilham896@gmail.com>
2025-12-14 20:46:29 +0700

    memperbaiki tampilan laporan part 2

--- application/views/backend/konsumen/v_konsumen.php
+++ application/views/backend/konsumen/v_konsumen.php
@@ -1,31 +1,38 @@
 <div class="container-fluid">
 
-        <div class= "row" > 
-            <div class= "col-md-12">
-                <a href="<?=base_url()?>konsumen/tambah" class =< btn btn-primary>Tambah Konsumen</a> <br><br>
+    <!-- Judul & Tombol -->
+    <div class="d-flex justify-content-between align-items-center mb-3">
+        <h1 class="h3 text-gray-800"><?= $judul; ?></h1>
+        <a href="<?= base_url('konsumen/tambah') ?>" class="btn btn-primary">
+            <i class="fas fa-plus"></i> Tambah Konsumen
+        </a>
+    </div>
+
+    <!-- Card -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary"><?= $judul; ?></h6>
         </div>
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800"><?= $judul;?></h1>
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary"><?= $judul;?></h6>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>No.</th>
-                                            <th>Kode</th>
-                                            <th>Nama Konsumen</th>
-                                            <th>Alamat</th>
-                                            <th>No. telepon</th>
-                                            <th>Opsi</th>
-                                        </tr>
-                                    </thead>
-                                </tbale>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            
+
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
+                    <thead class="thead-light">
+                        <tr class="text-center">
+                            <th width="5%">No.</th>
+                            <th>Kode</th>
+                            <th>Nama Konsumen</th>
+                            <th>Alamat</th>
+                            <th>No. Telepon</th>
+                            <th width="15%">Opsi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- isi data -->
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
