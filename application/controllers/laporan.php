6fcbba374b918d6c3216c9887e6c75a947971641
wahyu subehan <subehanwahyu26@gmail.com>
2025-12-17 00:04:02 +0700

    perbaikan laporan

--- application/controllers/laporan.php
+++ application/controllers/laporan.php
@@ -1,54 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Laporan Transaksi</title>
-    <style>
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-        }
-        .judul {
-            text-align: center;
-            margin-top: 30px;
-        }
-        .judul h2 {
-            margin-bottom: 5px;
-        }
-        .judul p {
-            font-size: 14px;
-        }
-        .filter {
-            text-align: center;
-            margin-top: 20px;
-        }
-    </style>
-</head>
-<body>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-<!-- FILTER -->
-<div class="filter">
-    <form method="get" action="">
-        Tanggal Mulai :
-        <input type="date" name="tgl_awal" required>
+class Laporan extends CI_Controller {
 
-        Tanggal Akhir :
-        <input type="date" name="tgl_akhir" required>
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_laporan');
+    }
 
-        <button type="submit">Tampilkan</button>
-    </form>
-</div>
+    public function index()
+    {
+        $isi['content'] = 'backend/laporan/f_laporan';
+        $isi['judul']   = 'Laporan Transaksi';
+        $this->load->view('backend/dashboard', $isi);
+    }
 
-<!-- JUDUL LAPORAN -->
-<?php if (!empty($_GET['tgl_awal']) && !empty($_GET['tgl_akhir'])): ?>
-<div class="judul">
-    <h2>Laporan Transaksi</h2>
-    <p>
-        Dari Tanggal 
-        <b><?= date('d F Y', strtotime($_GET['tgl_awal'])) ?></b>
-        Sampai Tanggal 
-        <b><?= date('d F Y', strtotime($_GET['tgl_akhir'])) ?></b>
-    </p>
-</div>
-<?php endif; ?>
-
-</body>
-</html>
+    public function cetak_laporan()
+    {
+        $tgl_mulai = $this->input->post('tanggal_mulai');
+        $tgl_akhir = $this->input->post('tanggal_akhir');
+        $isi['laporan'] = $this->m_laporan->filter_laporan($tgl_mulai, $tgl_akhir);
+		var_dump($isi['laporan']);
+        $this->load->view('backend/laporan/cetak_laporan');
+    }
+}
