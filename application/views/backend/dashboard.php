48adf056ac109986908a98b110419405f8b57897
AlfianIlham27 <alfianilham896@gmail.com>
2025-12-16 18:29:21 +0700

    update data konsumen part 3

--- application/views/backend/dashboard.php
+++ application/views/backend/dashboard.php
@@ -17,7 +17,7 @@
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-
+    <link rel="stylesheet" href="<?= base_url()?>assets/admin/vendor/datatables/dataTables.bootsrap4.min.css">
     <!-- Custom styles for this template-->
     <link href="<?= base_url()?>assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
 
@@ -179,6 +179,9 @@
     <!-- Page level plugins -->
     <script src="<?= base_url()?>assets/admin/vendor/chart.js/Chart.min.js"></script>
 
+    <script src="<?= base_url()?>assets/admin/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?= base_url()?>assets/admin/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="<?= base_url()?>assets/admin/js/demo/dataTables-demo.js"></script>
     <!-- Page level custom scripts -->
     <script src="<?= base_url()?>assets/admin/js/demo/chart-area-demo.js"></script>
     <script src="<?= base_url()?>assets/admin/js/demo/chart-pie-demo.js"></script>
--- application/views/backend/konsumen/v_konsumen.php
+++ application/views/backend/konsumen/v_konsumen.php
@@ -1,4 +1,19 @@
 <div class="container-fluid">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <?php
+        if (!empty($this->session->flashdata('info'))) {?>
+           <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Selamat!</strong> <?=$this->session->flashdata('info')?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+        <?php }
+    ?>
 
     <!-- Judul & Tombol -->
     <div class="d-flex justify-content-between align-items-center mb-3">
@@ -27,8 +42,24 @@
                             <th width="15%">Opsi</th>
                         </tr>
                     </thead>
+
                     <tbody>
-                        <!-- isi data -->
+                        <?php 
+                            $no = 1;
+                            foreach ($data as $row) {?>
+                                <tr>
+                                    <td><?= $no++; ?></td>
+                                    <td><?= $row->kode_konsumen;?></td>
+                                    <td><?= $row->nama_konsumen;?></td>
+                                    <td><?= $row->alamat_konsumen;?></td>
+                                    <td><?= $row->no_telp;?></td>
+                                    <td>
+                                        <a href="#" class="btn btn-success btn-sm"> Edit</a>
+                                        <a href="#" class="btn btn-danger btn-sm"> Delete</a>
+                                    </td>
+                                </tr>                            
+                            <?php }                        
+                        ?>
                     </tbody>
                 </table>
             </div>
