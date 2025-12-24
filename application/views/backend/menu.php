8d934fa94363b2a6da0e1395fb8fce0d2a714a1a
ilham115 <ilhammuhamad162@gmail.com>
2025-12-25 00:06:50 +0700

    by muha.ilham

--- application/views/backend/menu.php
+++ application/views/backend/menu.php
@@ -50,7 +50,7 @@
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="charts.html">
+                <a class="nav-link" href="<?= base_url()?>transaksi/riwayat">
                     <span>Riwayat Transaksi</span></a>
             </li>
 
